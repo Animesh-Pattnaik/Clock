a10895a2e9b0fb9d9644549d01a4ffdc1f896587
Animesh-Pattnaik <127220314+Animesh-Pattnaik@users.noreply.github.com>
2023-06-13 10:40:52 +0530

    Add files via upload

---
+++ stopwatch.js
@@ -0,0 +1,46 @@
+const timer = document.querySelector('.time');
+const start_button = document.getElementById('start');
+const stop_button = document.getElementById("stop");
+const reset_button = document.getElementById("reset");
+
+let seconds = 0;
+let interval = null;
+
+start_button.addEventListener('click', start);
+stop_button.addEventListener("click", stop);
+reset_button.addEventListener("click", reset);
+
+function update () {
+
+	let hrs = Math.floor(seconds / 3600);
+	let mins = Math.floor((seconds - (hrs * 3600)) / 60);
+	let secs = seconds % 60;
+
+    if (secs < 10) secs = '0' + secs;
+	if (mins < 10) mins = "0" + mins;
+	if (hrs < 10) hrs = "0" + hrs;
+
+    timer.innerText = `${hrs}:${mins}:${secs}`;
+
+    seconds++;
+}
+
+function start () {
+	if (interval) {
+		return
+	}
+
+	interval = setInterval(update, 1000);
+}
+
+function stop () {
+	clearInterval(interval);
+	interval = null;
+}
+
+function reset () {
+	stop();
+	seconds = 0;
+	timer.innerText = '00:00:00';
+}
+
---
+++ timer.js
@@ -0,0 +1,76 @@
+var start = document.getElementById('start');
+var stop = document.getElementById('stop');
+var reset = document.getElementById('reset');
+var stopsound = document.getElementById('stop-sound');
+
+var h = document.getElementById("hour");
+var m = document.getElementById("minute");
+var s = document.getElementById("second");
+
+var startTimer = null;
+var audio = document.getElementById("audio");
+
+stopsound.addEventListener('click',function(){
+    audio.pause();
+    audio.currentTime = 0;
+});
+
+start.addEventListener('click', function(){
+    if(startTimer === null && (h.value > 0 || m.value > 0 || s.value > 0)){
+        startTimer = setInterval(function() {
+            timer();
+        }, 1000);
+        h.disabled = true;
+        m.disabled = true;
+        s.disabled = true;
+    }
+});
+
+stop.addEventListener('click',function(){
+    clearInterval(startTimer);
+    startTimer = null;
+    h.disabled = true;
+    m.disabled = true;
+    s.disabled = true;
+});
+
+reset.addEventListener('click', function(){
+    h.value = 0;
+    m.value = 0;
+    s.value = 0;
+    stopInterval();
+    h.disabled = false;
+    m.disabled = false;
+    s.disabled = false;
+});
+
+function timer(){
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        m.value = 0;
+        s.value = 0;
+        stopInterval();
+        playAudio();
+    } else if(s.value != 0){
+        s.value--;
+    } else if(m.value != 0 && s.value == 0){
+        s.value = 59;
+        m.value--;
+    } else if(h.value != 0 && m.value == 0){
+        m.value = 60;
+        h.value--;
+    }
+}
+
+function stopInterval() {
+    clearInterval(startTimer);
+    startTimer = null;
+    h.disabled = false;
+    m.disabled = false;
+    s.disabled = false;
+}
+
+function playAudio(){
+        audio.play();
+}
+
